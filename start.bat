633e5b7cba530008a62acb2880408178a2889566
magikboy01 <magikboy01@verizon.net>
2016-02-18 13:35:46 -0500

    Uploaded unfinished source code to GitHub.
    + Unfinished Source
    - Unused methods

---
+++ start.bat
@@ -0,0 +1,107 @@
+@echo off
+color 0F
+goto Menu
+
+:Menu
+   echo Thanks for downloading User-Copy!
+   echo This tool is used for copying user folders to a hard-drive for safekeeping.
+   echo  
+   echo To get started, please type the name of the external hard-drive and press enter.
+   echo This is case-sensitive, so please type it identical to the name you see in the file explorer.
+   echo Example: C:\
+   echo  
+   set INPUT = 
+   set /P INPUT=Drive name: %=%
+   IF EXIST %INPUT%NUL goto valid
+   IF NOT EXIST %INPUT%NUL goto invalid
+   pause
+   
+:valid
+   color 0A
+   echo '
+   echo Entry valid!
+   sleep 2
+   goto colorWhite
+
+:invalid
+   color 0C
+   echo '
+   echo Entry Invalid!
+   sleep 2
+   goto retry
+
+:colorWhite
+   color 0F
+   goto username
+
+:retry
+   cls
+   color 0F
+   goto Menu
+
+:username
+   echo '
+   echo Type the username you want to copy.
+   set USER= 
+   set /P USER=Type username: %=%
+   IF EXIST C:\Users\%USER%\NUL goto userValid
+   IF NOT EXIST C:\Users\%USER%\NUL goto userInvalid
+
+:userInvalid
+   color 0C
+   echo Entry invalid!
+   sleep 2
+   color 0F
+   goto username
+
+:userValid
+   color 0A
+   echo Entry valid!
+   sleep 2
+   color 0F
+   goto usercontinue
+
+:usercontinue
+   set REENTER= 
+   set /P REENTER=Do you need to change the drive to copy to? (y/n) : %=%
+   IF /I %REENTER%==y set INPUT=Drive Name: %=%
+   IF /I %REENTER%==n goto userretry
+   IF EXIST %INPUT% (
+      color 0A
+      echo '
+      echo Entry valid!
+      sleep 2
+      color 0F
+   )
+   IF NOT EXIST %INPUT% (
+      color 0C
+      echo '
+      echo Entry invalid!
+      sleep 2
+      color 0F
+      goto usercontinue
+   )
+pause
+
+:userretry
+   set RENTER= 
+   set /P RENTER=Do you need to change the user folder to copy? (y/n) : %=%
+   IF /I %RENTER%==y set USER=Type username: %=%
+   IF /I %RENTER%==n echo Ok!
+
+   IF EXIST %USER% (
+      color 0A
+      echo '
+      echo Entry valid!
+      sleep 2
+      color 0F
+   )
+   IF NOT EXIST %USER% (
+      color 0C
+      echo '
+      echo Entry invalid!
+      sleep 2
+      color 0F
+      goto usercontinue
+   )
+pause
